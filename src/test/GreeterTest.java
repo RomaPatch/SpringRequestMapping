6c5fe520ad4f3dd744954cf136d98f6ca371ff3a
RomaPatch <roman.pons.ridaura@gmail.com>
2017-02-09 10:47:57 +0100

    Spring Request Mapping Example

---
+++ src/test/GreeterTest.java
@@ -0,0 +1,16 @@
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hello.Greeter;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+}
